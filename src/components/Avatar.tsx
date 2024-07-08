18ed2c22a3d29215892ba185c4b8b1b25404dca1
Alexander_Ovchar <azalexxx02@gmail.com>
2024-07-08 14:42:08 +0300

    feat: Add interactive button to change avatar animation

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,22 +1,40 @@
-import {useAnimations, useGLTF} from "@react-three/drei";
-import {useEffect} from "react";
+import {Html, useAnimations, useGLTF} from "@react-three/drei";
+import {useEffect, useState} from "react";
 
 export const Avatar = () => {
+  const [activeIndex, setActiveIndex] = useState(1);
+
   const avatar = useGLTF("/x-bot.glb");
 
   const {actions, names} = useAnimations(avatar.animations, avatar.scene);
-  console.log(actions, names)
+  console.log(actions, names);
+  
   useEffect(() => {
-    if (actions["rotateLeftHend"]) {
-      actions["rotateLeftHend"].play();
-    }
-  }, []);
+    actions[names[activeIndex]]?.reset().fadeIn(0.9).play();
+
+    return () => {
+      actions[names[activeIndex]]?.fadeOut(0.9);
+    };
+  }, [
+    activeIndex,
+    actions,
+    names,
+  ]);
 
   useEffect(() => {}, []);
 
   return (
     <group>
       <primitive object={avatar.scene} />
+      <Html 
+      position={[2, 0, 0]}
+      >
+        <button
+          onClick={() => {
+            setActiveIndex((prev) => (prev + 1) % names.length);
+          }}
+        >click me</button>
+      </Html>
     </group>
   );
 };
