6711df2b7e03689bc56d515d97553c49d314c929
Alexander_Ovchar <azalexxx02@gmail.com>
2024-07-08 12:58:14 +0300

    refactor: Update Avatar component to use useAnimations hook

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,22 @@
-import {useGLTF} from "@react-three/drei";
-import { useEffect } from "react";
+import {useAnimations, useGLTF} from "@react-three/drei";
+import {useEffect} from "react";
 
 export const Avatar = () => {
   const avatar = useGLTF("/x-bot.glb");
 
-  // const {actions, names } = useAnimations(avatar.animations, avatar.scene);
-
+  const {actions, names} = useAnimations(avatar.animations, avatar.scene);
+  console.log(actions, names)
   useEffect(() => {
-    
+    if (actions["rotateLeftHend"]) {
+      actions["rotateLeftHend"].play();
+    }
   }, []);
-  
 
-  return <group>
-    <primitive object={avatar.scene} />
-  </group>;
+  useEffect(() => {}, []);
+
+  return (
+    <group>
+      <primitive object={avatar.scene} />
+    </group>
+  );
 };
